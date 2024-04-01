49e5f88a5aaeaa974845581a4250463890ced2fb
Jasurbek <jasurbek.saydullayev@list.ru>
2024-04-01 21:43:51 +0500

    updated admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ProductImages, Product, Category, Shop
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description")
+    list_display_links = ('title',)
+    search_fields = ("id", "title", "parent__title")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description", "amount", "price", "active", "get_image")
+    list_display_links = ('title',)
+    search_fields = ("id", "title")
+
+    def get_image(self, product):
+        image = product.images.first()
+        if image:
+            return image.url
+
+
+@admin.register(ProductImages)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "url")
+    search_fields = ("id",)
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description", "image")
+    search_fields = ("id", "title")
--- shop/models.py
+++ shop/models.py
@@ -2,40 +2,64 @@ from django.db import models
 
 
 class Category(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    title = models.CharField(max_length=100, verbose_name="Называние")
+    description = models.TextField(verbose_name="Описание")
     parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
                                         related_name='children')
 
     class Meta:
-        verbose_name_plural = "categories"
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.title
 
 
 class Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    description = models.TextField()
-    title = models.CharField(max_length=100)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    images = models.ManyToManyField('ProductImages')
-    active = models.BooleanField(default=True)
-    category = models.ManyToManyField(Category, on_delete=models.CASCADE, related_name='products')
+    description = models.TextField(verbose_name="")
+    title = models.CharField(max_length=100, verbose_name="Называние")
+    amount = models.IntegerField(default=1, verbose_name="Количество")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    active = models.BooleanField(default=True, verbose_name="Активность")
+    category = models.ManyToManyField(Category, related_name='products',
+                                      verbose_name="Продукты")
+
+    class Meta:
+        verbose_name_plural = "Продукты"
+        verbose_name = "Продукт"
 
     def __str__(self):
         return self.title
 
 
 class ProductImages(models.Model):
-    id = models.AutoField(primary_key=True)
-    image = models.ImageField(upload_to='product_images/')
+    url = models.CharField(max_length=1024, verbose_name="Ссылка")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,
+                                verbose_name="Продукт", related_name="images")
+
+    class Meta:
+        verbose_name = "Изображения"
+        verbose_name_plural = "Изображение"
+
+    def __str__(self):
+        return self.product.title
 
 
 class Shop(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    image_url = models.URLField()
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="shops",
+        verbose_name="Продукт",
+    )
+    title = models.CharField(max_length=128, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(upload_to="shop_images/",
+                              verbose_name="Изоброжение")
+
+    class Meta:
+        verbose_name = "Магазин"
+        verbose_name_plural = "Магазины"
+
+    def __str__(self):
+        return self.title
