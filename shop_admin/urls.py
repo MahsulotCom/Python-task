63dd14ab28a47c875ecb35c8a97c30d7a748fd6c
KhayitovKhayitboy <kkhayitov@webster.edu>
2024-04-02 14:17:25 +0500

    created models, templates

--- shop_admin/urls.py
+++ shop_admin/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from .views import CategoryListView, CategoryUpdateView, CategoryDeleteView
 urlpatterns = [
-    
+    path('', CategoryListView.as_view(), name='home_page'), 
+    path('update/<int:pk>/', CategoryUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', CategoryDeleteView.as_view(), name='delete'),
 ]
--- shop_admin/views.py
+++ shop_admin/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from .models import Category
+from django.db.models import Q
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # Create your views here.
+
+
+
+class CategoryListView(ListView):
+    queryset=Category.objects.all()
+    template_name = 'base.html'
+    context_object_name = 'categories'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        queryset = Category.objects.all()
+        q = self.request.GET.get('q', '')
+        if q:
+            queryset = queryset.filter(Q(title__icontains=q) | Q(description__icontains=q))
+        context['categories'] = queryset
+        return context
+    
+class CategoryUpdateView(UpdateView):
+    queryset=Category.objects.all()
+    template_name='update.html'
+    fields=['title', 'description', 'parents']
+    success_url = reverse_lazy('home_page') 
+    
+ 
+class CategoryDeleteView(DeleteView):
+    model=Category
+    template_name='delete.html'
+    context_object_name='obj'
+    success_url = reverse_lazy('home_page') 
