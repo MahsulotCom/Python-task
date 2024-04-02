de03902994a1127147391a7173fcaa2bf162eb3f
Muhammadjon Muxamedov <muhammadjon.200415@gmail.com>
2024-04-02 11:47:20 +0500

    added shop field to user model

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -6,4 +6,11 @@ from apps.users.models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email", "shop")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
     readonly_fields = ("last_login", "date_joined", "created_at", "updated_at")
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from apps.common.models import TimeStampedModel
@@ -6,6 +7,14 @@ from apps.users.managers import UserManager
 
 
 class User(AbstractUser, TimeStampedModel):
+    shop = models.ForeignKey(
+        "main.Shop",
+        on_delete=models.SET_NULL,
+        related_name="users",
+        verbose_name=_("Shop"),
+        null=True,
+        blank=True,
+    )
     objects = UserManager()
 
     class Meta:
