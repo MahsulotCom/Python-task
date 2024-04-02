71339f3e1fa33f076af217ce6c25a19e2a1ca54e
KhayitovKhayitboy <kkhayitov@webster.edu>
2024-04-02 14:53:59 +0500

    created shop view, url

--- shop_admin/urls.py
+++ shop_admin/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CategoryListView, CategoryUpdateView, CategoryDeleteView
+from .views import CategoryListView, CategoryUpdateView, CategoryDeleteView, ShopListView, ShopUpdateView, ShopDeleteView
 urlpatterns = [
     path('', CategoryListView.as_view(), name='home_page'), 
     path('update/<int:pk>/', CategoryUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', CategoryDeleteView.as_view(), name='delete'),
+    path('shops/', ShopListView.as_view(), name='shops_page'), 
+    path('shops/update/<int:pk>/', ShopUpdateView.as_view(), name='update'),
+    path('shops/delete/<int:pk>/', ShopDeleteView.as_view(), name='delete'),
 ]
--- shop_admin/views.py
+++ shop_admin/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from .models import Category
+from .models import Category, Shop
 from django.db.models import Q
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-# Create your views here.
-
 
 
 class CategoryListView(ListView):
@@ -27,9 +25,35 @@ class CategoryUpdateView(UpdateView):
     fields=['title', 'description', 'parents']
     success_url = reverse_lazy('home_page') 
     
- 
 class CategoryDeleteView(DeleteView):
     model=Category
     template_name='delete.html'
     context_object_name='obj'
     success_url = reverse_lazy('home_page') 
+    
+class ShopListView(ListView):
+    queryset=Shop.objects.all()
+    template_name = 'shop.html'
+    context_object_name = 'shops'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        queryset = Shop.objects.all()
+        q = self.request.GET.get('q', '')
+        if q:
+            queryset = queryset.filter(Q(title__icontains=q) | Q(description__icontains=q))
+        context['shops'] = queryset
+        return context
+    
+class ShopUpdateView(UpdateView):
+    queryset=Shop.objects.all()
+    template_name='shop_update.html'
+    fields=['title', 'description', 'image_url']
+    success_url = reverse_lazy('shops_page') 
+    
+class ShopDeleteView(DeleteView):
+    model=Shop
+    template_name='delete.html'
+    context_object_name='obj'
+    success_url = reverse_lazy('shops_page') 
+    
