a49db44bdea9c1d438e16950e5860d9407aa56ff
Muhammadjon Muxamedov <muhammadjon.200415@gmail.com>
2024-04-02 22:52:18 +0500

    minor changes

--- apps/main/models.py
+++ apps/main/models.py
@@ -2,14 +2,9 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from mptt.fields import TreeForeignKey
-from mptt.models import MPTTModel, TreeManager
+from mptt.models import MPTTModel
 
 from apps.common.models import TimeStampedModel
-
-
-class CustomTreeManager(TreeManager):
-    def get_queryset(self):
-        return super().get_queryset().order_by("order", "title")
 
 
 class Category(MPTTModel, TimeStampedModel):
@@ -18,7 +13,6 @@ class Category(MPTTModel, TimeStampedModel):
     order = models.IntegerField(_("Order"), default=0)  # For ordering categories
     icon = models.FileField(_("Icon"), upload_to="icons/", null=True, blank=True)  # For category icons
     parent = TreeForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="children")
-    objects = CustomTreeManager()
 
     def __str__(self):
         return self.title
